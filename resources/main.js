50fc1966a345dff91bb7ad7ccbf7879861f1b1c5
Zach Hall <zach@zachhall.co>
2017-10-07 22:02:21 +0000

    get JSON via Github Gist instead of relying on repo

--- resources/main.js
+++ resources/main.js
@@ -9,7 +9,7 @@ new Vue({
     methods: {
         fetchData: function() {
             var self = this;
-            $.getJSON('https://raw.githubusercontent.com/zachhallco/learning_vuejs/master/resources/dealers.json', function(e) {
+            $.getJSON('https://gist.githubusercontent.com/zachhallco/965d7f039eac44f621840427bccc1ed7/raw/ff4110239d0fc39245ee2c528aa2d17148af25a7/poolpro_dealers.json', function(e) {
                 self.data = e.dealers;
                 console.log(self.data);
             });
